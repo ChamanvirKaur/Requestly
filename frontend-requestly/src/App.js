76cc0d2f8e610f073be1d66ffaca82538d991580
MeEtSoNi-lab <meet@tulong.tech>
2024-07-25 19:09:29 -0400

    Added Landing page component and style

--- frontend-requestly/src/App.js
+++ frontend-requestly/src/App.js
@@ -1,6 +1,7 @@
 import React,{useEffect} from 'react';
 import './App.css';
 import Typed from 'typed.js'; // before importing typed.js you need to install it using npm install typed.js
+import Landing from './Components/landing-page/Landing';
 
 function App() {
   useEffect(() => {
@@ -18,30 +19,9 @@ function App() {
     };
   }, []);
   return (
-    <>
-      <div className="body">
-        {/* header-text class contains the basic information and types of marketing request we provide */}
-        <div className="header-text">
-          {/* information about app */}
-          <div className="info-text">
-              <h1>Requestly</h1>
-              <p>Make any marketing related request with our simple and easy to understand platform <span>Requestly</span></p>
-          </div>
-          {/* types of service we provide */}
-          <div className="request-types-text">
-            <h3>Request you can send is <span className='auto-type'></span> Request </h3> 
-          </div>
-        </div>
+   <>
 
-        <div className="auth-buttons">
-          <div className="sign-up">
-            <button className='sign-up-button'>Sign-up</button>
-          </div>
-          <div className="login">
-            <button className='login-button'>Login</button>
-          </div>
-        </div>
-      </div>
+      <Landing/>
     </>
 
     
