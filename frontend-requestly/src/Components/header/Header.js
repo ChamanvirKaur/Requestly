341b14822412594bc549c04328541a70b6e5da08
MeEtSoNi-lab <meet@tulong.tech>
2024-08-09 16:31:12 -0400

    Chnaged login and signup design

--- frontend-requestly/src/Components/header/Header.js
+++ frontend-requestly/src/Components/header/Header.js
@@ -9,8 +9,8 @@ function Header() {
           <img className='header-logo-image' src= "./images/requestlylogo.png" alt="" />
 
               <ul id="sidemenu">
-                  <li><a href="#header">Login</a></li>
-                  <li><a href="#about">Signup</a></li>
+                  <li><a href="/login">Login</a></li>
+                  <li><a href="/signup">Signup</a></li>
                   <i class="fa-sharp fa-solid fa-xmark" onClick={()=>{document.getElementById("sidemenu").style.right="-500px"}} ></i>
               </ul>
               <i class="fa-solid fa-bars" onClick={()=>{document.getElementById("sidemenu").style.right="-200px"}}></i>
